993edd31bfcac38d6d65e7adfab318052b073ded
Scullyy <darrenscully25@yahoo.com>
2022-10-02 00:57:18 -0700

    Updated to ox

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,12 +2,11 @@ fx_version 'cerulean'
 game 'gta5'
 
 description 'QB-CityHall'
-version '2.0.0'
-
-ui_page 'html/index.html'
+version '3.0.0'
 
 shared_scripts {
     '@qb-core/shared/locale.lua',
+    '@ox_lib/init.lua',
     'locales/en.lua',
     'locales/*.lua',
     'config.lua'
@@ -19,12 +18,6 @@ client_scripts {
     '@PolyZone/client.lua',
     '@PolyZone/BoxZone.lua',
     'client/main.lua'
-}
-
-files {
-    'html/*.js',
-    'html/*.html',
-    'html/*.css'
 }
 
 lua54 'yes'
--- server/main.lua
+++ server/main.lua
@@ -33,10 +33,38 @@ local function giveStarterItems()
     end
 end
 
+local function getClosestHall(pedCoords)
+    local distance = #(pedCoords - Config.Cityhalls[1].coords)
+    local closest = 1
+    for i = 1, #Config.Cityhalls do
+        local hall = Config.Cityhalls[i]
+        local dist = #(pedCoords - hall.coords)
+        if dist < distance then
+            distance = dist
+            closest = i
+        end
+    end
+    return closest
+end
+
+local function getClosestSchool(pedCoords)
+    local distance = #(pedCoords - Config.DrivingSchools[1].coords)
+    local closest = 1
+    for i = 1, #Config.DrivingSchools do
+        local school = Config.DrivingSchools[i]
+        local dist = #(pedCoords - school.coords)
+        if dist < distance then
+            distance = dist
+            closest = i
+        end
+    end
+    return closest
+end
+
 -- Callbacks
 
-QBCore.Functions.CreateCallback('qb-cityhall:server:receiveJobs', function(_, cb)
-    cb(availableJobs)
+lib.callback.register('qb-cityhall:server:receiveJobs', function(source)
+    return availableJobs
 end)
 
 -- Events
@@ -46,7 +74,13 @@ RegisterNetEvent('qb-cityhall:server:requestId', function(item, hall)
     local Player = QBCore.Functions.GetPlayer(src)
     if not Player then return end
     local itemInfo = Config.Cityhalls[hall].licenses[item]
-    if not Player.Functions.RemoveMoney("cash", itemInfo.cost) then return TriggerClientEvent('QBCore:Notify', src, ('You don\'t have enough money on you, you need %s cash'):format(itemInfo.cost), 'error') end
+    if not Player.Functions.RemoveMoney("cash", itemInfo.cost) then 
+        return TriggerClientEvent('ox_lib:notify', src, {
+            title = 'Alert',
+            description = ('You don\'t have enough money on you, you need %s cash'):format(itemInfo.cost),
+            type = 'error'
+        })
+    end
     local info = {}
     if item == "id_card" then
         info.citizenid = Player.PlayerData.citizenid
@@ -69,12 +103,21 @@ RegisterNetEvent('qb-cityhall:server:requestId', function(item, hall)
     end
     if not Player.Functions.AddItem(item, 1, nil, info) then return end
     TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[item], 'add')
+    TriggerClientEvent('ox_lib:notify', src, {
+        title = 'Alert',
+        description = ('You have received your %s for $%s'):format(QBCore.Shared.Items[item].label, itemInfo.cost),
+        type = 'success'
+    })
 end)
 
-RegisterNetEvent('qb-cityhall:server:sendDriverTest', function(instructors)
+RegisterNetEvent('qb-cityhall:server:sendDriverTest', function()
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     if not Player then return end
+    local ped = GetPlayerPed(src)
+    local pedCoords = GetEntityCoords(ped)
+    local closestDrivingSchool = getClosestSchool(pedCoords)
+    local instructors = Config.DrivingSchools[closestDrivingSchool].instructors
     for i = 1, #instructors do
         local citizenid = instructors[i]
         local SchoolPlayer = QBCore.Functions.GetPlayerByCitizenId(citizenid)
@@ -90,21 +133,31 @@ RegisterNetEvent('qb-cityhall:server:sendDriverTest', function(instructors)
             TriggerEvent("qb-phone:server:sendNewMailToOffline", citizenid, mailData)
         end
     end
-    TriggerClientEvent('QBCore:Notify', src, "An email has been sent to driving schools, and you will be contacted automatically", "success", 5000)
+    TriggerClientEvent('ox_lib:notify', source, {
+        title = 'Alert',
+        description = "An email has been sent to driving schools, and you will be contacted automatically",
+        type = 'success'
+    })
 end)
 
-RegisterNetEvent('qb-cityhall:server:ApplyJob', function(job, cityhallCoords)
+RegisterNetEvent('qb-cityhall:server:ApplyJob', function(job)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     if not Player then return end
     local ped = GetPlayerPed(src)
     local pedCoords = GetEntityCoords(ped)
+    local closestCityhall = getClosestHall(pedCoords)
+    local cityhallCoords = Config.Cityhalls[closestCityhall].coords
     local JobInfo = QBCore.Shared.Jobs[job]
     if #(pedCoords - cityhallCoords) >= 20.0 or not availableJobs[job] then
         return DropPlayer(source, "Attempted exploit abuse")
     end
     Player.Functions.SetJob(job, 0)
-    TriggerClientEvent('QBCore:Notify', src, Lang:t('info.new_job', {job = JobInfo.label}))
+    TriggerClientEvent('ox_lib:notify', source, {
+        title = 'Alert',
+        description = Lang:t('info.new_job'),
+        type = 'success'
+    })
 end)
 
 RegisterNetEvent('qb-cityhall:server:getIDs', giveStarterItems)
@@ -121,16 +174,32 @@ QBCore.Commands.Add("drivinglicense", "Give a drivers license to someone", {{"id
                     if Config.DrivingSchools[i].instructors[id] == Player.PlayerData.citizenid then
                         SearchedPlayer.PlayerData.metadata["licences"]["driver"] = true
                         SearchedPlayer.Functions.SetMetaData("licences", SearchedPlayer.PlayerData.metadata["licences"])
-                        TriggerClientEvent('QBCore:Notify', SearchedPlayer.PlayerData.source, "You have passed! Pick up your drivers license at the town hall", "success", 5000)
-                        TriggerClientEvent('QBCore:Notify', source, ("Player with ID %s has been granted access to a driving license"):format(SearchedPlayer.PlayerData.source), "success", 5000)
+                        TriggerClientEvent('ox_lib:notify', source, {
+                            title = 'Alert',
+                            description = "You have passed! Pick up your drivers license at the town hall",
+                            type = 'success'
+                        })
+                        TriggerClientEvent('ox_lib:notify', source, {
+                            title = 'Alert',
+                            description = ("Player with ID %s has been granted access to a driving license"):format(SearchedPlayer.PlayerData.source),
+                            type = 'success'
+                        })
                         break
                     end
                 end
             end
         else
-            TriggerClientEvent('QBCore:Notify', source, "Can't give permission for a drivers license, this person already has permission", "error")
+            TriggerClientEvent('ox_lib:notify', source, {
+                title = 'Alert',
+                description = "Can't give permission for a drivers license, this person already has permission",
+                type = 'error'
+            })
         end
     else
-        TriggerClientEvent('QBCore:Notify', source, "Player Not Online", "error")
+        TriggerClientEvent('ox_lib:notify', source, {
+            title = 'Alert',
+            description = "Player Not Online",
+            type = 'error'
+        })
     end
 end)
