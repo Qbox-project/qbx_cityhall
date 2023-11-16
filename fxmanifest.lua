656af4af61d6f837bc2470f40e688e8d41586d30
Matthew <22198949+MafewTM@users.noreply.github.com>
2023-11-16 04:14:10 -0500

    fix: manifest update
    * fix: manifest update
    
    * fix: organize

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,9 @@
 fx_version 'cerulean'
 game 'gta5'
 
-description 'QBX-CityHall'
+description 'QBX_CityHall'
 repository 'https://github.com/Qbox-project/qbx_cityhall'
-version '3.0.0'
+version '1.0.0'
 
 modules {
     'qbx_core:utils',
@@ -11,21 +11,20 @@ modules {
 }
 
 shared_scripts {
-    '@qbx_core/shared/locale.lua',
     '@ox_lib/init.lua',
+    '@qbx_core/modules/utils.lua',
+    '@qbx_core/shared/locale.lua',
     'locales/en.lua',
     'locales/*.lua',
     'config.lua',
-    '@qbx_core/import.lua',
+}
+
+client_scripts {
+    '@qbx_core/modules/playerdata.lua',
+    'client/main.lua'
 }
 
 server_script 'server/main.lua'
 
-client_scripts {
-    '@PolyZone/client.lua',
-    '@PolyZone/BoxZone.lua',
-    'client/main.lua'
-}
-
 lua54 'yes'
-use_fxv2_oal 'yes'
+use_experimental_fxv2_oal 'yes'
