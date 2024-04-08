3af98c94fd1ff1b9b49beb21b5815ab90b8591b8
bicatcho11 <151503460+bicatcho11@users.noreply.github.com>
2024-04-08 23:22:23 +0300

    Update client.lua

--- config/client.lua
+++ config/client.lua
@@ -4,7 +4,7 @@ return {
 
     peds = {
         { -- Cityhall Ped
-            model = 'a_m_m_hasjew_01',
+            model = 'a_m_y_business_01',
             coords = vec4(-262.79, -964.18, 30.22, 181.71),
             scenario = 'WORLD_HUMAN_STAND_MOBILE',
             cityhall = true,
